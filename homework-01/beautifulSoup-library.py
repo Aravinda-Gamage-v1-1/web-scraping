bfcd6098183e6418c31c3f7077ea0d1ab3a39386
Aravinda-Gamage-v1-1 <aravindagamage21998@gmail.com>
2024-09-12 14:28:10 +0530

    beautifulSoup-library-homework

--- homework-01/beautifulSoup-library.py
+++ homework-01/beautifulSoup-library.py
@@ -1,11 +1,13 @@
 import requests
+from bs4 import BeautifulSoup
 
 # making a GET request
 response = requests.get('https://example.com/')
 
 # check status code for response received
-# success code - 200
+# success code = 200
 print(response)
 
-# print content of request
-print(response.content)
+# parsing the HTML
+soup = BeautifulSoup(response.content, 'html.parser')
+print(soup.prettify())
