46d167a2088e04bae0a87dccad6041d76e22a143
Aravinda-Gamage-v1-1 <aravindagamage21998@gmail.com>
2024-09-12 22:12:11 +0530

    change-url

--- homework-01/beautifulSoup-find-all.py
+++ homework-01/beautifulSoup-find-all.py
@@ -2,12 +2,12 @@ import requests
 from bs4 import BeautifulSoup
 
 # making a GET request
-response = requests.get('https://example.com/')
+response = requests.get('https://www.geeksforgeeks.org/fundamentals-of-algorithms/')
 
 # parsing the HTML
 soup = BeautifulSoup(response.content, 'html.parser')
 
-s = soup.find('div', class_ = 'entry-content')
+s = soup.find('div', class_ = 'text')
 content = s.find_all('p')
 
 print(content)
